3c81aec271d766b1f37e8f954b9b388529c657e5
KKODING <pkgore@naver.com>
2018-10-20 23:52:23 +0900

    10번., ~12번

--- code/practice_algorithm/practice_algorithm/10Gray code.cpp
+++ code/practice_algorithm/practice_algorithm/10Gray code.cpp
@@ -1,26 +1,53 @@
-///*
-//	Write a program that displays the normal binary representations,
-//	Gray code representations, and decoded Gray code values for all 5-bit numbers.
-//
-//*/
-//
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	int n = 5;
-//
-//
-//	cout << (n ) << endl;
-//	cout << (n >> 1) << endl;
-//	cout << (n >> 2) << endl;
-//	cout << (n >> 3) << endl;
-//	cout << (n >> 4) << endl;
-//
-//
-//
-//
-//
-//}
+/*
+	Write a program that displays the normal binary representations,
+	Gray code representations, and decoded Gray code values for all 5-bit numbers.
+
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n = 15;	//0000 0101
+
+	vector<int> v;
+	vector<int> gray_answer;
+	vector<int> originla_answer;
+	while (n != 0)
+	{
+		v.push_back(n & 1);
+		n = n >> 1;
+	}
+	v.push_back(0);
+
+	reverse(v.begin(), v.end());
+
+	/////////////////
+	gray_answer.push_back(v[0]);
+
+	for (int i = 0; i < v.size()-1; ++i) {
+		gray_answer.push_back(v[i] ^ v[i + 1]);
+	}
+
+	
+	cout << n << "ÀÇ Gray Code : ";
+	for (auto&d : gray_answer)
+		cout << d << ' ';
+	cout << endl;
+
+
+	originla_answer.push_back(gray_answer[0]);
+
+	for (int i = 0; i < v.size() - 1; ++i) {
+		originla_answer.push_back(originla_answer[i] ^ gray_answer[i + 1]);
+	}
+
+	cout << n << "ÀÇ Original Code : ";
+	for (auto&d : originla_answer)
+		cout << d << ' ';
+	cout << endl;
+
+
+}
